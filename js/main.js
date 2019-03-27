1cf7518c5beabeb0f1325a03875f72f8c4307f1c
Chitoku <odango@chitoku.jp>
2019-03-28 03:29:41 +0900

    Update http-vue-loader

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ require.config({
         'babel-polyfill': 'https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.26.0/polyfill.min',
         'vue': 'https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.6/vue',
         'vuex': 'https://cdnjs.cloudflare.com/ajax/libs/vuex/3.0.1/vuex.min',
-        'http-vue-loader': 'https://cdn.rawgit.com/chitoku-k/http-vue-loader/bc8e142332b8f396715ec3fc61fbad03ae2a8040/src/httpVueLoader',
+        'http-vue-loader': 'https://cdn.jsdelivr.net/npm/http-vue-loader@1.4.0/src/httpVueLoader.min',
     },
 });
 require(['babel!app']);
